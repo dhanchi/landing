a562b19371c6c9cc589c5b4369a51c5c2dc51140
Prasan1 <paul.paudyal@gmail.com>
2023-01-30 13:55:19 -0500

    feat:description/contact page

--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from  home import settings
 from django.http import HttpResponse
+from .forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+
 
 
 def home(request):
-    return render(request, 'index.html')
+    if request.method=='POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = "Website Inquiry" 
+            body = {
+                    'name': form.cleaned_data['name'], 
+                    'subject': form.cleaned_data['subject'], 
+                    'email': form.cleaned_data['email'], 
+                    'message':form.cleaned_data['message'], 
+			}
+            message = "\n".join(body.values())
+            sender = form.cleaned_data['email']
+            name = form.cleaned_data['name']
+            subject = form.cleaned_data['subject'], 
+            msg = f"""Sender: {name}<{sender}>,
+                      subject': {subject}, 
+                    Message: {body.get('message')}"""
+            
+            send_mail(subject=body.get('subject').strip(),message=msg, from_email=body.get('email'), recipient_list=['dhanchillc@gmail.com'] ) 
+            return redirect ("success")
+    form = ContactForm()
+    return render(request, 'index.html', {'form':form})
 
-# def about(request):
-#     return render(request, 'index.html')
-
-# def contact(request):
-#     form = contactForm()
-#     context = {
-#         'form':form,
-#     }
-#     return render(request, "contact.html", context)
+def success(request):
+    return render(request, 'contact.html')
